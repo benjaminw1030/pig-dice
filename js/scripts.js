af8a0b41309ce97e21f1810e4e3a936e776c9a07
Ben Wilson <benjaminw1030@gmail.com>
2021-08-25 14:52:13 -0700

    add dice images Co-authored-by: Mark Chang <marksrc7@gmail.com> Co-authored-by: Chris Nakayama <cnakayam@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -37,63 +37,81 @@ Player.prototype.winCheck = function () {
 
 let player1 = new Player("", 0, 0);
 let player2 = new Player("", 0, 0);
+let currentPlayer = 1
+
+function rollOne() {
+  let hideButton = "#player-" + currentPlayer + "-roll"
+  $(hideButton).hide();
+}
 
 function playerOneEnd() {
-  $("#player-1-play").fadeOut();
+  $("#player-1-roll").show();
+  $("#player-1-play").hide();
+  $("#p2-dice-1").show();
   if (player1.winCheck() === true) {
     $("#win-display").fadeIn();
     $("#winner").text(player1.name);
   } else {
     $("#player-2-play").fadeIn();
   }
+  currentPlayer = 2;
 }
 
 function playerTwoEnd() {
-  $("#player-2-play").fadeOut();
+  $("#player-2-roll").show();
+  $("#player-2-play").hide();
+  $("#p1-dice-1").show();
   if (player2.winCheck() === true) {
     $("#win-display").fadeIn();
     $("#winner").text(player2.name);
   } else {
     $("#player-1-play").fadeIn();
   }
+  currentPlayer = 1;
 }
 
-  function attachButtonRollListeners() {
+function attachButtonRollListeners() {
 
-    $("button#player-1-roll").on("click", function () {
-      let roll = player1.roll();
-      $("#player-1-current").text(player1.currentScore);
-      if (roll === 1) {
-        playerOneEnd();
-      }
-    });
+  $("button#player-1-roll").on("click", function () {
+    let roll = player1.roll();
+    let diceId = "#p1-dice-" + roll;
+    $("#player-1-current").text(player1.currentScore);
+    if (roll === 1) {
+      rollOne();
+    }
+    $(".dice-img").hide();
+    $(diceId).fadeIn();
+  });
 
-    $("button#player-2-roll").on("click", function () {
-      let roll = player2.roll();
-      $("#player-2-current").text(player2.currentScore);
-      if (roll === 1) {
-        playerTwoEnd();
-      }
-    });
+  $("button#player-2-roll").on("click", function () {
+    let roll = player2.roll();
+    let diceId = "#p2-dice-" + roll;
+    $("#player-2-current").text(player2.currentScore);
+    if (roll === 1) {
+      rollOne();
+    }
+    $(".dice-img").hide();
+    $(diceId).fadeIn();
+  });
 
-    $("button#player-1-hold").on("click", function () {
-      player1.endTurn();
-      $("#player-1-current").text(player1.currentScore);
-      $("#player-1-total").text(player1.totalScore);
-      playerOneEnd();
-    });
+  $("button#player-1-hold").on("click", function () {
+    player1.endTurn();
+    $("#player-1-current").text(player1.currentScore);
+    $("#player-1-total").text(player1.totalScore);
+    playerOneEnd();
+  });
 
-    $("button#player-2-hold").on("click", function () {
-      player1.endTurn();
-      $("#player-2-current").text(player2.currentScore);
-      $("#player-2-total").text(player2.totalScore);
-      playerTwoEnd();
-    });
+  $("button#player-2-hold").on("click", function () {
+    player2.endTurn();
+    $("#player-2-current").text(player2.currentScore);
+    $("#player-2-total").text(player2.totalScore);
+    playerTwoEnd();
+  });
 
-    $("button#play-again").on("click", function () {
-      location.reload();
-    });
-  }
+  $("button#play-again").on("click", function () {
+    location.reload();
+  });
+}
 
 $(document).ready(function () {
   attachButtonRollListeners();
@@ -103,9 +121,9 @@ $(document).ready(function () {
     const p2 = $("input#player-2").val();
     player1.name = p1;
     player2.name = p2;
-    $("input#player-1").val("");
-    $("input#player-2").val("");
-    $("form#name-entry").fadeOut();
+    $("#player-1-name").text(p1);
+    $("#player-2-name").text(p2);
+    $("form#name-entry").hide();
     $("#player-1-play").fadeIn();
   });
 });
