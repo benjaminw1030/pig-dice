fe4330f42a249894819b797eb8d2efd95de4d9eb
Ben Wilson <benjaminw1030@gmail.com>
2021-08-25 13:28:31 -0700

    add UI logic, MVP o-authored-by: Mark Chang <marksrc7@gmail.com> Co-authored-by: Chris Nakayama <cnakayam@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,27 @@ function Player(name, currentScore, totalScore) {
 }
 
 function rollDice() {
-  return Math.ceil(Math.random()*6);
+  return Math.ceil(Math.random() * 6);
 }
 
-Player.prototype.endTurn = function() {
+Player.prototype.endTurn = function () {
   this.totalScore += this.currentScore;
   this.currentScore = 0;
 }
 
-Player.prototype.roll = function() {
+Player.prototype.roll = function () {
   const roll = rollDice();
-  console.log(roll);
   if (roll === 1) {
     this.currentScore = 0;
     this.endTurn();
   } else {
     this.currentScore += roll;
   }
+  return roll;
 }
 
-Player.prototype.winCheck = function() {
-  if (player.totalScore >= 100) {
+Player.prototype.winCheck = function () {
+  if (this.totalScore >= 100) {
     return true
   }
   return false
@@ -35,48 +35,65 @@ Player.prototype.winCheck = function() {
 
 // UI Logic
 
-function attachButtonRollListeners () {
+let player1 = new Player("", 0, 0);
+let player2 = new Player("", 0, 0);
 
-  $("button#player-1-roll").on("click", function() {
-    player1.roll();
-    $("#player-1-current").text(player1.currentScore);
-  });
-  
-  $("button#player-2-roll").on("click", function() {
-    player2.roll();
-    $("#player-2-current").text(player2.currentScore);
-  });
-
-  $("button#player-1-hold").on("click", function() {
-    player1.endTurn();
-    $("#player-1-current").text(player1.currentScore);
-    $("#player-1-total").text(player1.totalScore);
-    $("#player-1-play").fadeOut();
-    if (player1.winCheck() === true) {
-      $("#win-display").fadeIn();
-      $("#winner").text(player1.name);
-    } else {
-      $("#player-2-play").fadeIn();
-    }
-  });
-
-  $("button#player-2-hold").on("click", function() {
-    player1.endTurn();
-    $("#player-2-current").text(player2.currentScore);
-    $("#player-2-total").text(player2.totalScore);
-    $("#player-2-play").fadeOut();
-    if (player2.winCheck() === true) {
-      $("#win-display").fadeIn();
-      $("#winner").text(player2.name);
-    } else {
-      $("#player-2-play").fadeIn();
-    }
-  });
-
-  $("button#play-again").on("click", function() {
-     location.reload();
-  });
+function playerOneEnd() {
+  $("#player-1-play").fadeOut();
+  if (player1.winCheck() === true) {
+    $("#win-display").fadeIn();
+    $("#winner").text(player1.name);
+  } else {
+    $("#player-2-play").fadeIn();
+  }
 }
+
+function playerTwoEnd() {
+  $("#player-2-play").fadeOut();
+  if (player2.winCheck() === true) {
+    $("#win-display").fadeIn();
+    $("#winner").text(player2.name);
+  } else {
+    $("#player-1-play").fadeIn();
+  }
+}
+
+  function attachButtonRollListeners() {
+
+    $("button#player-1-roll").on("click", function () {
+      let roll = player1.roll();
+      $("#player-1-current").text(player1.currentScore);
+      if (roll === 1) {
+        playerOneEnd();
+      }
+    });
+
+    $("button#player-2-roll").on("click", function () {
+      let roll = player2.roll();
+      $("#player-2-current").text(player2.currentScore);
+      if (roll === 1) {
+        playerTwoEnd();
+      }
+    });
+
+    $("button#player-1-hold").on("click", function () {
+      player1.endTurn();
+      $("#player-1-current").text(player1.currentScore);
+      $("#player-1-total").text(player1.totalScore);
+      playerOneEnd();
+    });
+
+    $("button#player-2-hold").on("click", function () {
+      player1.endTurn();
+      $("#player-2-current").text(player2.currentScore);
+      $("#player-2-total").text(player2.totalScore);
+      playerTwoEnd();
+    });
+
+    $("button#play-again").on("click", function () {
+      location.reload();
+    });
+  }
 
 $(document).ready(function () {
   attachButtonRollListeners();
@@ -84,10 +101,10 @@ $(document).ready(function () {
     event.preventDefault();
     const p1 = $("input#player-1").val();
     const p2 = $("input#player-2").val();
+    player1.name = p1;
+    player2.name = p2;
     $("input#player-1").val("");
     $("input#player-2").val("");
-    let player1 = new Player(p1, 0, 0);
-    let player2 = new Player(p2, 0, 0);
     $("form#name-entry").fadeOut();
     $("#player-1-play").fadeIn();
   });
