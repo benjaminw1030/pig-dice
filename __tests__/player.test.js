d80d6decfbe5cbd86006d23b64639e0a8355fa17
Ben Wilson <benjaminw1030@gmail.com>
2021-08-31 10:59:53 -0700

    pass endTurn test Co-authored-by: MarkKu7 <cameron.kronsbein@gmail.com>

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,10 +1,15 @@
 import Player from './../src/player.js';
 
-describe  ("Player", () => {
-  test ("should create a player with a name current score and total score",() => {
+describe ("Player", () => {
+  test("should create a player with a name current score and total score",() => {
     const player = new Player ("Cam", 11, 23);
       expect(player.name).toEqual("Cam");
       expect(player.currentScore).toEqual(11);
       expect(player.totalScore).toEqual(23);
+  });
+  test("Should add current score to total score", () => {
+    const player = new Player ("Cam", 11, 23);
+    player.endTurn();
+    expect(player.totalScore).toEqual(34);
   });
 });
